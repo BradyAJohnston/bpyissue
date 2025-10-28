688d7dbe1f802c7ac223e6aba147f7bfbca12228
Brady Johnston <brady.johnston@me.com>
2025-10-28 10:26:22 -0700

    inital commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import bpy
+
+def _update_handler(self, context: bpy.types.Context) -> None:
+    print("Updating property!")
+
+def main():
+    bpy.types.Object.custom_property = bpy.props.FloatProperty()
+    bpy.types.Object.custom_property = bpy.props.FloatProperty(update=_update_handler)
+    obj = bpy.data.objects["Cube"]
+    print(obj)
+
+
+if __name__ == "__main__":
+    main()
