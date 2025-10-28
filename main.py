b88d609c11a1abc22fcce6f2a1f90a5d05e8bb59
Brady Johnston <36021261+BradyAJohnston@users.noreply.github.com>
2025-10-28 10:46:56 -0700

    update handler

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ def _update_handler(self, context: bpy.types.Context) -> None:
     print("Updating property!")
 
 def main():
-    bpy.types.Object.custom_property = bpy.props.FloatProperty()
     bpy.types.Object.custom_property = bpy.props.FloatProperty(update=_update_handler)
     obj = bpy.data.objects["Cube"]
     print(obj)
